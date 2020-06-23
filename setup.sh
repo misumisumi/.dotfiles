5b1b7bad61e65605319c2883978e15611cb22541
Kobayashi Sumiharu <dragon511southern@gmail.com>
2020-06-24 02:37:49 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -1,8 +1,18 @@
 #/bin/bash
-DOTPATH=$HOME/dotfiles
+DOTPATH=$HOME/.dotfiles
 for f in .??*
 do
     [ "$f" = ".git" ] && continue
-    echo $f
-    # ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
+    [ "$f" = ".zplug" ] && continue
+    [ "$f" = ".config" ] && continue
+    ln -snfv "$DOTPATH/$f" "$HOME/$f"
+    # echo "$DOTPATH/$f"
+    # echo "$HOME/$f"
+done
+
+for f in $(ls $DOTPATH/.config)
+do
+    ln -snfv "$DOTPATH/.config/$f" "$HOME/.config/$f"
+    # echo "$DOTPATH/.config/$f"
+    # echo "$HOME/.conifg/$f"
 done
