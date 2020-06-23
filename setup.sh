784245cb63d56c567db64b7c240c29c7c71aa6d9
Kobayashi Sumiharu <dragon511southern@gmail.com>
2020-06-24 00:19:06 +0900

    Updates

--- setup.sh
+++ setup.sh
@@ -1,13 +1,8 @@
 #/bin/bash
-DOT_FILES=(.bash_profile .bashrc .gtkrc-2.0 .xinitrc .xprofile .xsessionrc .xmonad/xmonad.hs)
-DOT_DIRS=(.config/alacritty .config/gtk-3.0 .config/polybar .config/rofi)
-
-for file in ${DOT_FILES[@]}
+DOTPATH=$HOME/dotfiles
+for f in .??*
 do
-    ln -s $HOME/dotfiles/$file $HOME/$file
-done
-
-for dir in ${DOT_DIRS[@]}
-do
-    ln -s $HOME/dotfiles/$dir $HOME/$dir
+    [ "$f" = ".git" ] && continue
+    echo $f
+    # ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
 done
