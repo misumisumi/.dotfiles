a1a4f8215110353840b585a64851b808bf9bfb46
Kobayashi Sumiharu <dragon511southern@gmail.com>
2020-06-24 12:30:08 +0900

    update

--- setup.sh
+++ setup.sh
@@ -3,12 +3,13 @@ DOTPATH=$HOME/.dotfiles
 for f in .??*
 do
     [ "$f" = ".git" ] && continue
-    [ "$f" = ".zplug" ] && continue
     [ "$f" = ".config" ] && continue
     ln -snfv "$DOTPATH/$f" "$HOME/$f"
     # echo "$DOTPATH/$f"
     # echo "$HOME/$f"
 done
+
+ln -snfv "$DOTPATH/.vimrc" "$HOME/.config/nvim/init.vim"
 
 for f in $(ls $DOTPATH/.config)
 do
